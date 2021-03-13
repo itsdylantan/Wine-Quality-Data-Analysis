81896a0459fb3dea8926ae2f3146b56cd0a41def
ethaneen <ethan.nathanael@gmail.com>
2021-03-12 20:52:51 -0800

    Changed the raw data because csv did not work

---
+++ analysis/scripts/.ipynb_checkpoints/functions-checkpoint.py
@@ -0,0 +1,26 @@
+
+def load_and_process(url_or_path_to_csv_file):
+​
+    # Method Chain 1 (Load data, deal with missing data, and make data readable)
+​
+    df1 = (
+          pd.read_csv(url_or_path_to_csv_file)
+          .dropna()
+          .sort_values("alcohol", ascending=False)
+          .sort_values("fixed acidity", ascending=False)
+          .reset_index(drop=True)
+      )
+​
+    # Method Chain 2 (Create new columns, drop others, and do processing)
+​
+     df2 = (
+          df1
+          .rename(columns={"free sulfur dioxide": "free SO2"})
+          .rename(columns={"total sulfur dioxide": "total SO2"})
+          .assign(High_quality = lambda x: x['quality']>4)
+          .loc[df1['residual sugar']>15]
+      )
+​
+    # Make sure to return the latest dataframe
+​
+    return df2
