a95d22770b8c8ad1785a2cc51c1a6e8f9176aba1
iwanlevin <apple.iwan@gmail.com>
2021-03-14 08:46:36 -0700

    slight edit to data cleaning funtions

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -17,18 +17,19 @@ def load_and_process(url_or_path_to_csv_file):
 # Method Chain 2 (Create new columns, drop others, and do processing)
 
     df2 = (df1
-       .rename(columns={"free sulfur dioxide": "free SO2"})
-       .rename(columns={"total sulfur dioxide": "total SO2"})
-       .assign(High_quality = lambda x: x['quality']>4)
-       .loc[df1['residual sugar']>15]
+        .rename(columns={"free sulfur dioxide": "free SO2"})
+        .rename(columns={"total sulfur dioxide": "total SO2"})
+        .assign(high_quality = lambda x: x['quality']>4) #removes outliers from quality
+        .loc[df1['residual sugar']<20] #removes outliers from residual sugars
+        .loc[df1['residual sugar']>2]
       )
-
 # Make sure to return the latest dataframe
     return df2
 
 def refine_data(dataframe): #Refine the dataset down by removing outiers and 
     dataframe = (dataframe[dataframe['high_quality']==True]
                  .drop(columns = ['high_quality'])
+                 .reset_index() # resets the index for the data set after removing rows during data cleaning
                  )
  
     return dataframe
